21cb14a8b69e3dc1a58158756f5bcd9579e86139
iitaiy <iitaiy@gmail.com>
2016-11-22 19:36:11 +0200

    second commit

--- app/src/main/java/com/keeptrip/keeptrip/MainActivity.java
+++ app/src/main/java/com/keeptrip/keeptrip/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     // kodus!!!!
     // at lo hayevet li klum
+    // fjfsdakfsda
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
