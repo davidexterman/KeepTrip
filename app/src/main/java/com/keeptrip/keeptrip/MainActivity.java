6b9eae5741b7b871b66b6391821865465ee848ae
Exterman David <sdavidex@campus.technion.ac.il>
2016-11-29 17:23:50 +0200

    changing gitignore

--- app/src/main/java/com/keeptrip/keeptrip/MainActivity.java
+++ app/src/main/java/com/keeptrip/keeptrip/MainActivity.java
@@ -4,8 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    // kodus!!!!
-    // at lo hayevet li klum
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
