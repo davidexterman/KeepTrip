0190c6a6d8d1a723fe7f57cd5210efda0a1023d4
iitaiy <iitaiy@gmail.com>
2016-11-30 22:21:15 +0200

    SqlDataProvider Mock

---
+++ app/src/main/java/com/keeptrip/keeptrip/TripMainActivity.java
@@ -0,0 +1,13 @@
+package com.keeptrip.keeptrip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TripMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_main);
+    }
+}
