52f9f35405eb8d30f1ad93b6e8c719e13540de8f
oritmussel <oritmussel@gmail.com>
2016-12-02 14:58:32 +0200

    Trip Create Update

--- app/src/main/java/com/keeptrip/keeptrip/MainActivity.java
+++ app/src/main/java/com/keeptrip/keeptrip/MainActivity.java
@@ -1,7 +1,9 @@
 package com.keeptrip.keeptrip;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.MainToolBar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        Intent intent = new Intent(this, TripCreateActivity.class);
+        startActivity(intent);
     }
 }
