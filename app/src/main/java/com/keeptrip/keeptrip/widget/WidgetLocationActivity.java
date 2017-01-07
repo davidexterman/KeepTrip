552d15e434ee7aaac530ea026bd7f48f0e37fcdd
oritmussel <oritmussel@gmail.com>
2017-01-07 12:05:58 +0200

    location widget no trips handle

---
+++ app/src/main/java/com/keeptrip/keeptrip/widget/WidgetLocationActivity.java
@@ -0,0 +1,74 @@
+package com.keeptrip.keeptrip.widget;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.keeptrip.keeptrip.R;
+import com.keeptrip.keeptrip.contentProvider.KeepTripContentProvider;
+import com.keeptrip.keeptrip.dialogs.NoTripsDialogFragment;
+import com.keeptrip.keeptrip.model.Landmark;
+import com.keeptrip.keeptrip.model.Trip;
+import com.keeptrip.keeptrip.utils.DateUtils;
+import com.keeptrip.keeptrip.utils.DbUtils;
+
+import java.util.Calendar;
+
+public class WidgetLocationActivity extends Activity implements NoTripsDialogFragment.NoTripDialogClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widget_location);
+
+        if(savedInstanceState == null) {
+
+                Trip lastTrip = DbUtils.getLastTrip(this);
+                if(lastTrip == null){
+                    NoTripsDialogFragment dialogFragment = new NoTripsDialogFragment();
+
+                    Bundle args = new Bundle();
+                    args.putInt(dialogFragment.CALLED_FROM_WHERE_ARGUMENT, dialogFragment.CALLED_FROM_ACTIVITY);
+                    dialogFragment.setArguments(args);
+                    dialogFragment.show(getFragmentManager(), "noTrips");
+                }
+
+                else {
+                    addLocationLandmark();
+                }
+        }
+    }
+
+    private void addLocationLandmark() {
+        Trip lastTrip = DbUtils.getLastTrip(this);
+        if(lastTrip != null) {
+            Landmark newLandmark = new Landmark(lastTrip.getId(),
+                    "My Landmark", "", DateUtils.getDateOfToday(), "", new Location(""), "", 0);
+
+            // Insert data to DataBase
+            getContentResolver().insert(
+                    KeepTripContentProvider.CONTENT_LANDMARKS_URI,
+                    newLandmark.landmarkToContentValues());
+
+            Toast.makeText(this, getResources().getString(R.string.toast_landmark_added_message_success), Toast.LENGTH_SHORT).show();
+            finishAffinity();
+        }
+    }
+
+    @Override
+    public void onClickHandleParent(int whichButton, String newTripTitle) {
+        NoTripsDialogFragment.DialogOptions whichOptionEnum = NoTripsDialogFragment.DialogOptions.values()[whichButton];
+        switch (whichOptionEnum){
+            case DONE:
+                Trip newTrip = new Trip(newTripTitle, Calendar.getInstance().getTime(), "", "", "");
+                DbUtils.addNewTrip(this, newTrip);
+                addLocationLandmark();
+                break;
+            case CANCEL:
+                Toast.makeText(this, getResources().getString(R.string.toast_no_trips_dialog_canceled_message), Toast.LENGTH_LONG).show();
+                finishAffinity();
+        }
+    }
+}
--- app/src/main/java/com/keeptrip/keeptrip/widget/WidgetLocationProvider.java
+++ app/src/main/java/com/keeptrip/keeptrip/widget/WidgetLocationProvider.java
@@ -1,21 +1,17 @@
 package com.keeptrip.keeptrip.widget;
 
 
-import android.app.Activity;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.os.Bundle;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.keeptrip.keeptrip.R;
 import com.keeptrip.keeptrip.contentProvider.KeepTripContentProvider;
-import com.keeptrip.keeptrip.dialogs.NoTripsDialogActivity;
-import com.keeptrip.keeptrip.dialogs.NoTripsDialogFragment;
 import com.keeptrip.keeptrip.model.Landmark;
 import com.keeptrip.keeptrip.model.Trip;
 import com.keeptrip.keeptrip.utils.DateUtils;
@@ -33,10 +29,15 @@ public class WidgetLocationProvider extends AppWidgetProvider {
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
                 R.layout.widget_location);
 
-        Intent intent = new Intent(context, WidgetLocationProvider.class);
-        intent.setAction(ADD_LOCATION_LANDMARK);
-        pendingIntent = PendingIntent.getBroadcast(context,
-                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        // Create an Intent to launch WidgetLocationActivity
+        Intent intent = new Intent(context, WidgetLocationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+
+//        Intent intent = new Intent(context, WidgetLocationProvider.class);
+//        intent.setAction(ADD_LOCATION_LANDMARK);
+//        pendingIntent = PendingIntent.getBroadcast(context,
+//                0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         for (int i = 0; i < count; i++) {
             int widgetId = appWidgetIds[i];
@@ -46,39 +47,34 @@ public class WidgetLocationProvider extends AppWidgetProvider {
         }
     }
 
-    @Override
-    public void onReceive(Context context, Intent intent) {
-
-        if (intent.getAction().equals(ADD_LOCATION_LANDMARK)) {
-
-            Trip lastTrip = DbUtils.getLastTrip(context);
-            if(lastTrip == null){
-//                NoTripsDialogFragment dialogFragment = new NoTripsDialogFragment();
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
 //
-//                Bundle args = new Bundle();
-//                args.putInt(dialogFragment.CALLED_FROM_WHERE_ARGUMENT, dialogFragment.CALLED_FROM_FRAGMENT);
-//                dialogFragment.setArguments(args);
-
-                Intent dialogIntent = new Intent(context, NoTripsDialogActivity.class);
-                dialogIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(dialogIntent);
-            }
-
-            //TODO:get result (new trip)
-            if(lastTrip != null) {
-                Landmark newLandmark = new Landmark(lastTrip.getId(),
-                        "My Landmark", "", DateUtils.getDateOfToday(), "", new Location(""), "", 0);
-
-                // Insert data to DataBase
-                context.getContentResolver().insert(
-                        KeepTripContentProvider.CONTENT_LANDMARKS_URI,
-                        newLandmark.landmarkToContentValues());
-
-                Toast.makeText(context, context.getResources().getString(R.string.toast_landmark_added_message_success), Toast.LENGTH_SHORT).show();
-            }
-        }
-
-   //     }
-        super.onReceive(context, intent);
-    }
+//        if (intent.getAction().equals(ADD_LOCATION_LANDMARK)) {
+//
+//            Trip lastTrip = DbUtils.getLastTrip(context);
+//            if(lastTrip == null){
+//
+//                Intent dialogIntent = new Intent(context, WidgetLocationActivity.class);
+//                dialogIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                context.startActivity(dialogIntent);
+//            }
+//
+//            //TODO:get result (new trip)
+//            if(lastTrip != null) {
+//                Landmark newLandmark = new Landmark(lastTrip.getId(),
+//                        "My Landmark", "", DateUtils.getDateOfToday(), "", new Location(""), "", 0);
+//
+//                // Insert data to DataBase
+//                context.getContentResolver().insert(
+//                        KeepTripContentProvider.CONTENT_LANDMARKS_URI,
+//                        newLandmark.landmarkToContentValues());
+//
+//                Toast.makeText(context, context.getResources().getString(R.string.toast_landmark_added_message_success), Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//
+//   //     }
+//        super.onReceive(context, intent);
+//    }
 }
