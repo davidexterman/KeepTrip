f64102e3c11b14569dc9ebdfaf331ef6bdaf00fd
Exterman David <sdavidex@campus.technion.ac.il>
2017-01-10 18:07:06 +0200

    finalized landmark maps and location handleing

--- app/src/main/java/com/keeptrip/keeptrip/landmark/activity/LandmarkMultiMap.java
+++ app/src/main/java/com/keeptrip/keeptrip/landmark/activity/LandmarkMultiMap.java
@@ -1,7 +1,6 @@
 package com.keeptrip.keeptrip.landmark.activity;
 
-import android.content.res.TypedArray;
-import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -13,7 +12,6 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.keeptrip.keeptrip.R;
 
@@ -35,14 +33,22 @@ public class LandmarkMultiMap extends LandmarkMap {
     public void onMapReady(GoogleMap googleMap) {
         super.onMapReady(googleMap);
         int index = 0;
-        LatLng landmarkLatLng;
+        LatLng landmarkLatLng = null;
 
         if (lmArrayList.isEmpty()){
-            Toast.makeText(this, R.string.no_landmarks_to_view, Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.no_landmarks_to_view, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.make_sure_gps_enabled_or_map, Toast.LENGTH_LONG).show();
             return;
         }
 
+        Location currentLocation;
         do{
+            currentLocation = lmArrayList.get(index).getGPSLocation();
+            if(currentLocation == null){
+                index ++;
+                continue;
+            }
+
             landmarkLatLng = new LatLng(
                     lmArrayList.get(index).getGPSLocation().getLatitude(),
                     lmArrayList.get(index).getGPSLocation().getLongitude()
@@ -63,37 +69,27 @@ public class LandmarkMultiMap extends LandmarkMap {
             index ++;
         } while (index < lmArrayList.size());
 
-//        points.add(new LatLng(32,35)); mMap.addMarker(new MarkerOptions().position(new LatLng(32,35)));
-//        points.add(new LatLng(32.2,35.1)); mMap.addMarker(new MarkerOptions().position(new LatLng(32.2,35.1)));
-//        points.add(new LatLng(32.6,35.15)); mMap.addMarker(new MarkerOptions().position(new LatLng(32.6,35.15)));
-//        points.add(new LatLng(32.45,35.25));mMap.addMarker(new MarkerOptions().position(new LatLng(32.45,35.25)));
-//        points.add(new LatLng(31.80,34.9));mMap.addMarker(new MarkerOptions().position(new LatLng(31.80,34.9)));
-//        points.add(new LatLng(31.10,35.8));mMap.addMarker(new MarkerOptions().position(new LatLng(31.10,35.8)));
-//        points.add(new LatLng(30,32));mMap.addMarker(new MarkerOptions().position(new LatLng(30,32)));
-//        points.add(new LatLng(32,35));mMap.addMarker(new MarkerOptions().position(new LatLng(32,35)));
-//        points.add(new LatLng(34,38));mMap.addMarker(new MarkerOptions().position(new LatLng(34,38)));
-//        points.add(new LatLng(35,39));mMap.addMarker(new MarkerOptions().position(new LatLng(35,39)));
-//        points.add(new LatLng(35,40));mMap.addMarker(new MarkerOptions().position(new LatLng(35,40)));
+        if(landmarkLatLng == null){
+            Toast.makeText(this, R.string.no_landmarks_with_location_found, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.make_sure_gps_enabled_or_map, Toast.LENGTH_LONG).show();
+        }else {
 
+            CameraUpdate cu;
+            if (markers.size() == 1) {
+                cu = CameraUpdateFactory.newLatLngZoom(landmarkLatLng, 15);
+            } else {
+                mMap.addPolyline(new PolylineOptions()
+                        .addAll(points)
+                        .width(5)
+                        .color(R.color.accent));
 
+                LatLngBounds bounds = getMarkersBound(markers);
+                cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
+            }
 
-        CameraUpdate cu;
-        if(markers.size() == 1){
-            cu = CameraUpdateFactory.newLatLngZoom(landmarkLatLng,15);
+            // move the camera
+            mMap.animateCamera(cu, 2000, null);
         }
-        else{
-            mMap.addPolyline(new PolylineOptions()
-                    .addAll(points)
-                    .width(5)
-                    .color(R.color.accent));
-
-            LatLngBounds bounds = getMarkersBound(markers);
-            cu = CameraUpdateFactory.newLatLngBounds(bounds, 100);
-        }
-
-        // move the camera
-        mMap.animateCamera(cu, 2000, null);
-
     }
 
     LatLngBounds getMarkersBound(ArrayList<Marker> markers){
--- app/src/main/java/com/keeptrip/keeptrip/utils/LocationUtils.java
+++ app/src/main/java/com/keeptrip/keeptrip/utils/LocationUtils.java
@@ -1,8 +1,34 @@
 package com.keeptrip.keeptrip.utils;
 
+import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.util.Log;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 /**
  * Created by omussel on 1/6/2017.
  */
 
 public class LocationUtils {
+
+    public static void updateLmLocationString(Activity activity, EditText lmEditText, Location location){
+        Geocoder gcd = new Geocoder(activity, Locale.getDefault());
+        try { //TODO: lat and lng will be 0 if nothing has changed when location isn't on (and not returning null)
+            List<Address> addresses = gcd.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+            if (addresses.size() > 0) {
+                Address ad = addresses.get(0);
+                String locationName = ad.getAddressLine(0) != null ? ad.getAddressLine(0) :
+                        (ad.getLocality() != null ? ad.getLocality() : ad.getCountryName());
+                lmEditText.setText(locationName);
+            }
+        } catch (IOException e) {
+            Log.i(activity.getLocalClassName(), "IOException = " + e.getCause());
+        }
+    }
 }
