40ef5b2464d91d3fa94118e80b0d8c997c24acba
Exterman David <sdavidex@campus.technion.ac.il>
2017-01-26 16:11:40 +0200

    fixed open map after location permission with no gps on create landmark

--- app/src/main/java/com/keeptrip/keeptrip/landmark/fragment/LandmarkDetailsFragment.java
+++ app/src/main/java/com/keeptrip/keeptrip/landmark/fragment/LandmarkDetailsFragment.java
@@ -1126,6 +1126,10 @@ public class LandmarkDetailsFragment extends Fragment implements
                     getLmAutomaticLocationErrorTextView,
                     mLastLocation,
                     lmAutomaticLocationTextView.getText().toString());
+            if(isMapClicked){
+                isMapClicked = false;
+                startGoogleMapIntent();
+            }
         }
     }
 
